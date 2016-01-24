34cdf837aaa5aa561240ad9a6580616a85750f42
liuhao <liuhaoduoduo@163.com>
2016-01-24 15:22:38 +0800

    新增：设置是背景Drawable对象Api兼容代码

---
+++ api_adaptive/src/main/java/space/liuhao/api_adaptive/Background.java
@@ -0,0 +1,29 @@
+package space.liuhao.api_adaptive;
+
+import android.annotation.TargetApi;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.view.View;
+
+/**
+ * 北京类Api兼容
+ *
+ * @author 刘浩 2016-01-24 15-14
+ * @version 1.0.0
+ */
+public class Background {
+    /**
+     * 设置是背景Drawable对象
+     * @param view 待设置的视图对象
+     * @param drawable Drawable对象
+     */
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    public static final void setBackgroundDrawable(View view,Drawable drawable){
+        if(Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1<Build.VERSION.SDK_INT){
+            //noinspection deprecation
+            view.setBackgroundDrawable(drawable);
+        }else{
+            view.setBackground(drawable);
+        }
+    }
+}
